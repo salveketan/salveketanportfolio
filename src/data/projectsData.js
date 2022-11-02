ef32ca91063164c65be89f1832c22cbc0148461e
salveketan <salveketan6@gmail.com>
2022-11-02 11:07:26 +0530

    kfc changes

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,9 +3,26 @@ import KFC from "../assets/png/KFC.png";
 // import codecov from "../assets/png/codecov.png";
 // import makemytrip from "../assets/png/makemytrip.png";
 export const projectsData = [
+
   {
     id: 1,
-    projectName: "SEPHORA-CLONE",
+    projectName: 'KFC-Clone',
+    projectDesc: [
+      `A web application which is build using MERN`,
+      [
+        "You can add Product to cart, Sing-Up and Log-in",
+        "User Authentication, Chakra-UI"
+      ],
+    ],
+    tags: ['React', 'Express', 'MongoDB', "Node js"],
+    code: 'https://github.com/salveketan/kfcfrontend',
+    demo: 'https://kfc-dummy.vercel.app/',
+    image: KFC,
+  },
+
+  {
+    id: 2,
+    projectName: "SEPHORA-Clone",
     projectDesc: [
       `This project is clone of Sephora Cosmatics, E-commerce website of Cosmatic brand.`,
       [
@@ -19,8 +36,8 @@ export const projectsData = [
     image: "https://camo.githubusercontent.com/05f1e2e9486c4a95da79557efcf6d452a5bccdcd69a7167f2396f139647b152f/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f3837352f312a576f4b33367742624242685a5f5572347044777473672e706e67",
   },
   {
-    id: 2,
-    projectName: "Pepperfry",
+    id: 3,
+    projectName: "Pepperfry-clone",
     projectDesc: [
       `A web application is an online furniture and home decor shopping store.`,
       [
@@ -28,25 +45,10 @@ export const projectsData = [
         "Created a Wishlist Page"
       ],
     ],
-    tags: ['HTML', 'CSS', 'JavaScript', 'Express', 'MongoDB', "Node js"],
+    tags: ['JavaScript', 'Express', 'MongoDB', "Node js"],
     code: "https://github.com/NishutSuman/Pepperfry_Clone",
     demo: "https://pepperfry-clone.netlify.app/",
     image: "https://miro.medium.com/max/875/0*idWzYwb3FeoXZPK5.png",
-  },
-  {
-    id: 3,
-    projectName: 'KFC-Clone',
-    projectDesc: [
-      `A web application which is build using MERN`,
-      [
-        "You can add Product to cart, Sing-Up and Log-in",
-        "User Authentication, Chakra-UI"
-      ],
-    ],
-    tags: ['HTML', 'CSS', 'JavaScript', 'Express', 'MongoDB', "Node js"],
-    code: 'https://github.com/salveketan/KFCproject',
-    demo: 'https://kfc-project-ketan.vercel.app/',
-    image: KFC,
   },
 
   {
