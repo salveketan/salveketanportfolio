9048c997868d8997643c56dc71781fb23b886f64
ketan <97458783+salveketan@users.noreply.github.com>
2023-10-06 19:08:35 +0530

    Update experienceData.js
    EXPERINECE

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,24 @@
 export const experienceData = [
-  {
+   {
     id: 1,
+    company: "PREFLIGHT AVI SERVICES",
+    jobtitle: "FULL STACK DEVELOPER",
+    startYear: "NOV 2022",
+    endYear: "PRESENT",
+  },
+   {
+    id: 2,
+    company: "WNS Global Services ",
+    jobtitle: "Analyst",
+    startYear: "2021",
+    endYear: "2021",
+  }
+  {
+    id: 3,
     company: "Raychem,RPG",
     jobtitle: "Service and Maintenance ",
     startYear: "2019",
     endYear: "2021",
   },
-  {
-    id: 1,
-    company: "WNS Global Services ",
-    jobtitle: "Analyst",
-    startYear: "2021",
-    endYear: "2021",
-  },
-  {
-    id: 1,
-    company: "Masai",
-    jobtitle: "Full Stack Web Development",
-    startYear: "2021",
-    endYear: "present",
-  },
+ 
 ];
