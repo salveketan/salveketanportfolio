146305793b0ae8f971e7c6abcc2055b1959952b7
ketan <97458783+salveketan@users.noreply.github.com>
2023-10-06 19:09:09 +0530

    Update educationData.js
    EDUCTAION

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -6,23 +6,6 @@ export const educationData = [
     place: "Sangmaner, Ahemadnagar",
     startYear: "2016",
     endYear: "2019",
-  },
- 
-  {
-    id: 2,
-    institution: "Bhujbal Knowledge City – MET League of Colleges - Nashik",
-    course: "Diploma in Electrical",
-    place: " Nashik, Maharashtra",
-    startYear: "2013",
-    endYear: "2016",
-  },
-  {
-    id: 3,
-    institution: "Masai School of Coding",
-    course: "Fullstack Web-Development",
-    place: " Bengaluru,Karnataka",
-    startYear: "2021",
-    endYear: "Present",
-  },
+  }
   
 ];
