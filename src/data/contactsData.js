5fa524644fc20efa02d85911d4079bcc1b4d773d
ketan <97458783+salveketan@users.noreply.github.com>
2023-10-06 19:57:53 +0530

    Update contactsData.js
    address

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
   email: "salveketan6@gmail.com",
   phone: "+91 9545260659 / 7972765737",
-  address: "Nashik, Maharashtra",
+  address: "Banglore, Karnataka",
 
   sheetAPI: "",
 };
